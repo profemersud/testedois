272015f57eb38a8ffac0f7eee87ae03085cd7f25
profemersud <emerson.md.magalhaes@gmail.com>
2025-05-14 12:23:26 -0300

    adicionando linha no ex 4

--- src/pctAula9ex4/Exemplo4.java
+++ src/pctAula9ex4/Exemplo4.java
@@ -5,6 +5,7 @@ public class Exemplo4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ola mundo");
+		System.out.println("outra linha alterada");
 
 	}
 
